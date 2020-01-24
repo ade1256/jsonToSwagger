17c516f875f8f2f73f6ed930065d88bd80b51369
ade1256 <adeprasetyo174@gmail.com>
2020-01-24 19:01:22 +0700

    add component

---
+++ src/Components/Repository/Repository.component.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const RepositoryComponent = () => {
+  return (
+    <div>
+      <p>
+        Paste your schema and then we will generate your schema into readable
+        type data structure. This is very useful when you need documentation
+        about your endpoint. Swagger allows you to describe the structure of
+        your APIs so that machines can read them. The ability of APIs to
+        describe their own structure is the root of all awesomeness in Swagger.
+      </p>
+      <h3>Awesome Team</h3>
+      <p>
+        <a href="https://github.com/mufti1">Mufti Ismi Rizqi (Backend)</a>
+      </p>
+      <p>
+        <a href="https://github.com/ade1256">Ade Prasetyo (Frontend)</a>
+      </p>
+      <h3>Repositories</h3>
+      <a href="https://github.com/ade1256/jsonToSwagger">
+        https://github.com/ade1256/jsonToSwagger
+      </a>
+      <br />
+      <a href="https://github.com/mufti1">https://github.com/mufti1</a>
+    </div>
+  );
+};
+
+export default RepositoryComponent;
--- src/Pages/Home/Home.component.js
+++ src/Pages/Home/Home.component.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ConvertForm } from '../../Containers';
+import { Repository } from '../../Components';
 
 const HomeComponent = () => {
   return (
     <div>
       <ConvertForm />
+      <Repository />
     </div>
   );
 };
