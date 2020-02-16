6a04220a0decd2c8e132590ef8d138a0ceba7342
mufti <mufti.rizqi@go-jek.com>
2020-02-16 11:14:43 +0700

    [Mufti] Add unique visitor counter

--- src/Components/Repository/Repository.component.js
+++ src/Components/Repository/Repository.component.js
@@ -22,7 +22,9 @@ const RepositoryComponent = () => {
         https://github.com/ade1256/jsonToSwagger (frontend)
       </a>
       <br />
-      <a href="https://github.com/mufti1/json-to-swagger">https://github.com/mufti1/json-to-swagger (backend)</a>
+      <a href="https://github.com/mufti1/json-to-swagger">https://github.com/mufti1/json-to-swagger (backend)</a> <br/> <br/>
+      <h3>Page Views: </h3>
+      <img src="https://hitwebcounter.com/counter/counter.php?page=7195916&style=0030&nbdigits=5&type=ip&initCount=0" title="User Stats" Alt="webcounterwebsite"   border="0" />
     </div>
   );
 };
